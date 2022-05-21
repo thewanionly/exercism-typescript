334a49f832c290aad23d1bb2f6dea0da3967c2df
thewanionly <wani.pitogo@gmail.com>
2022-05-22 04:37:02 +0800

    Solved `resistor-color-duo` exercise

--- resistor-color-duo/resistor-color-duo.ts
+++ resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,18 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+enum ResistorColorValues {
+  black,
+  brown,
+  red,
+  orange,
+  yellow,
+  green,
+  blue,
+  violet,
+  grey,
+  white
+}
+
+type ResistorColor = keyof typeof ResistorColorValues
+
+export function decodedValue([firstBand, secondBand]: ResistorColor[]): number {
+  return ResistorColorValues[firstBand] * 10 + ResistorColorValues[secondBand]
 }
