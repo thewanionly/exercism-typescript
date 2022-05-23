20410f474929c94e921f74cd7d3d378563d01fed
thewanionly <wani.pitogo@gmail.com>
2022-05-23 23:47:48 +0800

    Solved `resistor-color-trio` exercise

--- resistor-color-trio/resistor-color-trio.ts
+++ resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,21 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+enum ResistorColorValues {
+  black,
+  brown,
+  red,
+  orange,
+  yellow,
+  green,
+  blue,
+  violet,
+  grey,
+  white
+}
+
+type ResistorColor = keyof typeof ResistorColorValues
+
+export function decodedResistorValue([firstBand, secondBand, thirdBand]: ResistorColor[]): string {
+  return `${
+    (ResistorColorValues[firstBand] * 10 + ResistorColorValues[secondBand]) *
+    10 ** ResistorColorValues[thirdBand]
+  } ohms`.replace('000 ', ' kilo')
 }
