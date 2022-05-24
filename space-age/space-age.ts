5fc671ea0737a09e2cdacff57ec2dd1fe0d874b9
thewanionly <wani.pitogo@gmail.com>
2022-05-25 00:26:10 +0800

    Solved `space-age` exercise

--- space-age/space-age.ts
+++ space-age/space-age.ts
@@ -1,3 +1,21 @@
-export function age(planet: unknown, seconds: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+enum OrbitalPeriod {
+  mercury = 0.2408467,
+  venus = 0.61519726,
+  earth = 1,
+  mars = 1.8808158,
+  jupiter = 11.862615,
+  saturn = 29.447498,
+  uranus = 84.016846,
+  neptune = 164.79132
+}
+
+const EARTH_SECONDS_PER_YEAR = 31557600
+
+type Planet = keyof typeof OrbitalPeriod
+
+export function age(planet: Planet, seconds: number): number {
+  const planetAge = seconds / (OrbitalPeriod[planet] * EARTH_SECONDS_PER_YEAR)
+  const planteAge2Dec = Math.round((planetAge + Number.EPSILON) * 100) / 100
+
+  return planteAge2Dec
 }
