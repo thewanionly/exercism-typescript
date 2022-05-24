7c2d79cd8562b8aae303be53c58be5c62c306cb5
thewanionly <wani.pitogo@gmail.com>
2022-05-24 23:43:06 +0800

    Solved `rna-transcription` exercise

--- rna-transcription/rna-transcription.ts
+++ rna-transcription/rna-transcription.ts
@@ -1,3 +1,26 @@
-export function toRna() {
-  throw new Error('Remove this statement and implement this function')
+const DNAtoRNAMapping: { [key: string]: string } = {
+  G: 'C',
+  C: 'G',
+  T: 'A',
+  A: 'U'
+} as const
+
+type DNANucleotide = keyof typeof DNAtoRNAMapping
+
+type RNANucleotide<T> = T[keyof T]
+
+export function toRna(DNAStrand: string): string {
+  const DNAStrandArray: string[] = DNAStrand.split('')
+
+  // Check if input is valid
+  if (DNAStrandArray.some((nucleotide) => !Object.keys(DNAtoRNAMapping).includes(nucleotide))) {
+    // Throw error if invalid
+    throw new Error('Invalid input DNA.')
+  }
+
+  // Determine RNA complement if valid
+  return DNAStrandArray.map(
+    (nucleotide: DNANucleotide): RNANucleotide<typeof DNAtoRNAMapping> =>
+      DNAtoRNAMapping[nucleotide]
+  ).join('')
 }
