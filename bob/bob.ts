19e09140cbd8c85a86780902af3eb09e6d21e6df
thewanionly <wani.pitogo@gmail.com>
2022-05-27 04:41:03 +0800

    Solved `bob` exercise Iteration 2 (index based approach, much cleaner)

--- bob/bob.ts
+++ bob/bob.ts
@@ -1,43 +1,43 @@
-const BobResponse = {
-  question: 'Sure.',
-  yell: 'Whoa, chill out!',
-  yell_question: `Calm down, I know what I'm doing!`,
-  nothing: 'Fine. Be that way!',
-  other: 'Whatever.'
-} as const
+const BobResponse = [
+  'Whatever.',
+  'Sure.',
+  'Whoa, chill out!',
+  `Calm down, I know what I'm doing!`,
+  'Fine. Be that way!'
+] as const
+
+enum MessageType {
+  other,
+  question,
+  yell,
+  yell_quesion,
+  nothing
+}
 
 type Response<T> = T[keyof T]
 
 export function hey(message: string): Response<typeof BobResponse> {
   const incomingMessage = message.replace(/\s/g, '') // remove all whitespaces
-  let response: Response<typeof BobResponse> = BobResponse.other // set other as default
+  let responseIndex = MessageType.other // set other as default
 
-  // for nothing, check if message is empty
+  // If empty, set to nothing
   if (!incomingMessage) {
-    response = BobResponse.nothing
-  } else {
-    // for question, check if end char is "?"
-    if (incomingMessage.slice(-1) === '?') {
-      response = BobResponse.question
-    }
-
-    // for yelling, check if all letters is upper case
-    if (
-      incomingMessage.toUpperCase() === incomingMessage &&
-      incomingMessage.toLowerCase() !== incomingMessage
-    ) {
-      response = BobResponse.yell
-    }
-
-    // for yelling + questioning, check if all letters is upper case and has "?" at the end
-    if (
-      incomingMessage.slice(-1) === '?' &&
-      incomingMessage.toUpperCase() === incomingMessage &&
-      incomingMessage.toLowerCase() !== incomingMessage
-    ) {
-      response = BobResponse.yell_question
-    }
+    responseIndex = MessageType.nothing
   }
 
-  return response
+  // If question, set to question
+  if (incomingMessage.slice(-1) === '?') {
+    responseIndex = MessageType.question
+  }
+
+  // If yelling, if current index is already set previously, set to yell_question. Otherwise, set to yell
+  if (
+    incomingMessage.toUpperCase() === incomingMessage &&
+    incomingMessage.toLowerCase() !== incomingMessage
+  ) {
+    responseIndex = responseIndex ? MessageType.yell_quesion : MessageType.yell
+  }
+
+  // return the corresponding response based on the responseIndex
+  return BobResponse[responseIndex]
 }
