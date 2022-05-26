67d952b711cd9a38ab9e25bdf4876b4483d75067
thewanionly <wani.pitogo@gmail.com>
2022-05-27 01:15:22 +0800

    Solved `pangram` exercise

--- pangram/pangram.test.ts
+++ pangram/pangram.test.ts
@@ -5,43 +5,39 @@ describe('Pangram()', () => {
     expect(isPangram('')).toBe(false)
   })
 
-  xit('perfect lower case', () => {
+  it('perfect lower case', () => {
     expect(isPangram('abcdefghijklmnopqrstuvwxyz')).toBe(true)
   })
 
-  xit('only lower case', () => {
+  it('only lower case', () => {
     expect(isPangram('the quick brown fox jumps over the lazy dog')).toBe(true)
   })
 
-  xit("missing the letter 'x'", () => {
-    expect(
-      isPangram('a quick movement of the enemy will jeopardize five gunboats')
-    ).toBe(false)
+  it("missing the letter 'x'", () => {
+    expect(isPangram('a quick movement of the enemy will jeopardize five gunboats')).toBe(false)
   })
 
-  xit("missing the letter 'h'", () => {
+  it("missing the letter 'h'", () => {
     expect(isPangram('five boxing wizards jump quickly at it')).toBe(false)
   })
 
-  xit('with underscores', () => {
+  it('with underscores', () => {
     expect(isPangram('the_quick_brown_fox_jumps_over_the_lazy_dog')).toBe(true)
   })
 
-  xit('with numbers', () => {
-    expect(isPangram('the 1 quick brown fox jumps over the 2 lazy dogs')).toBe(
-      true
-    )
+  it('with numbers', () => {
+    expect(isPangram('the 1 quick brown fox jumps over the 2 lazy dogs')).toBe(true)
   })
 
-  xit('missing letters replaced by numbers', () => {
+  it('missing letters replaced by numbers', () => {
     expect(isPangram('7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog')).toBe(false)
   })
 
-  xit('mixed case and punctuation', () => {
+  it('mixed case and punctuation', () => {
     expect(isPangram('"Five quacking Zephyrs jolt my wax bed."')).toBe(true)
   })
 
-  xit('case insensitive', () => {
+  it('case insensitive', () => {
     expect(isPangram('the quick brown fox jumps over with lazy FX')).toBe(false)
   })
 })
--- pangram/pangram.ts
+++ pangram/pangram.ts
@@ -1,3 +1,20 @@
-export function isPangram() {
-  throw new Error('Remove this statement and implement this function')
+const MIN_CHAR_CODE = 97 //char code of 'a'
+const MAX_CHAR_CODE = 122 //char code of 'z'
+const TOTAL_ALPHABET_LETTERS = 26
+
+export function isPangram(sentence: string): boolean {
+  // Change all characters to lowercase. Remove characters that are not a letter
+  let sentenceArray = sentence
+    .toLowerCase()
+    .split('')
+    .filter(
+      (character) =>
+        character.charCodeAt(0) >= MIN_CHAR_CODE && character.charCodeAt(0) <= MAX_CHAR_CODE
+    )
+
+  // Remove duplicates
+  sentenceArray = [...new Set(sentenceArray)]
+
+  // Check length. If it is 26, then all alphabet letters are in the array hence it is a pangram
+  return sentenceArray.length === TOTAL_ALPHABET_LETTERS
 }
