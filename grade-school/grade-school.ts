1d9742d8b51211401399e628a16944af2592ab14
thewanionly <wani.pitogo@gmail.com>
2022-05-28 05:41:36 +0800

    Solved `grade-school` exercise Iteration 2 (technically 3) (removeDuplicates fn, use for in loop, access this.roster() in grade fn, use JSON parse/stringify for deep clone)

--- grade-school/grade-school.ts
+++ grade-school/grade-school.ts
@@ -9,24 +9,11 @@ export class GradeSchool {
 
   roster(): Roster {
     // Return a deep copy of schoolRoster to make schoolRoster immutable from outisde updates
-    return Object.fromEntries(
-      Object.entries(this.schoolRoster).map(([key, value]) => [key, [...value]])
-    )
+    return JSON.parse(JSON.stringify(this.schoolRoster))
   }
 
   add(name: string, grade: number): void {
-    // Check if the name already exist in other grades. If so, remove the name from the previous grade
-    const gradesList: number[] = Object.keys(this.schoolRoster).map((key: string) => Number(key))
-
-    for (let i = 0; i < gradesList.length; i++) {
-      if (this.schoolRoster[gradesList[i]]?.includes(name)) {
-        this.schoolRoster[gradesList[i]] = this.schoolRoster[gradesList[i]].filter(
-          (studentName: string) => studentName !== name
-        )
-
-        break
-      }
-    }
+    this.removeDuplicates(name)
 
     if (this.schoolRoster[grade]) {
       // Add new name then sort alphabetically
@@ -39,6 +26,19 @@ export class GradeSchool {
   }
 
   grade(grade: number): string[] {
-    return this.schoolRoster[grade] ? [...this.schoolRoster[grade]] : []
+    return this.roster()[grade] || []
+  }
+
+  private removeDuplicates(name: string): void {
+    // Check if the name already exist in other grades. If so, remove the name from the previous grade
+    for (let grade in this.schoolRoster) {
+      if (this.schoolRoster[grade]?.includes(name)) {
+        this.schoolRoster[grade] = this.schoolRoster[grade].filter(
+          (studentName: string) => studentName !== name
+        )
+
+        break
+      }
+    }
   }
 }
