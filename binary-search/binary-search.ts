5e9e186fadd8811d39031c6c0af53677291f542c
thewanionly <wani.pitogo@gmail.com>
2022-05-29 04:53:38 +0800

    Solved `binary-search` exercise Iteration 1

--- binary-search/binary-search.ts
+++ binary-search/binary-search.ts
@@ -1,3 +1,21 @@
-export function find(haystack: unknown, needle: unknown): number | never {
-  throw new Error('Remove this statement and implement this function')
+export function find(haystack: number[], needle: number): number | never {
+  let pivot: number = Math.floor(haystack.length / 2)
+  let left = 0
+  let right: number = haystack.length - 1
+
+  while (haystack[pivot] !== needle && pivot > left && pivot < right) {
+    if (haystack[pivot] > needle) {
+      right = pivot - 1
+    } else {
+      left = pivot + 1
+    }
+
+    pivot = Math.floor((right - left) / 2) + left
+  }
+
+  if (haystack[pivot] === needle) {
+    return pivot
+  } else {
+    throw new Error('Value not in array')
+  }
 }
