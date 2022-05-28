a329c1048e30324fd7ee8384edd980354c1eb39d
thewanionly <wani.pitogo@gmail.com>
2022-05-29 04:10:04 +0800

    Solved `secret-handshake` exercise

--- secret-handshake/secret-handshake.test.ts
+++ secret-handshake/secret-handshake.test.ts
@@ -6,53 +6,43 @@ describe('Secret Handshake', () => {
       expect(commands(1)).toEqual(['wink'])
     })
 
-    xit('double blink for 10', () => {
+    it('double blink for 10', () => {
       expect(commands(2)).toEqual(['double blink'])
     })
 
-    xit('close your eyes for 100', () => {
+    it('close your eyes for 100', () => {
       expect(commands(4)).toEqual(['close your eyes'])
     })
 
-    xit('jump for 1000', () => {
+    it('jump for 1000', () => {
       expect(commands(8)).toEqual(['jump'])
     })
 
-    xit('combine two actions', () => {
+    it('combine two actions', () => {
       expect(commands(3)).toEqual(['wink', 'double blink'])
     })
 
-    xit('reverse two actions', () => {
+    it('reverse two actions', () => {
       expect(commands(19)).toEqual(['double blink', 'wink'])
     })
 
-    xit('reversing one action gives the same action', () => {
+    it('reversing one action gives the same action', () => {
       expect(commands(24)).toEqual(['jump'])
     })
 
-    xit('reversing no actions still gives no actions', () => {
+    it('reversing no actions still gives no actions', () => {
       expect(commands(16)).toEqual([])
     })
 
-    xit('all possible actions', () => {
-      expect(commands(15)).toEqual([
-        'wink',
-        'double blink',
-        'close your eyes',
-        'jump',
-      ])
+    it('all possible actions', () => {
+      expect(commands(15)).toEqual(['wink', 'double blink', 'close your eyes', 'jump'])
     })
 
-    xit('reverse all possible actions', () => {
-      expect(commands(31)).toEqual([
-        'jump',
-        'close your eyes',
-        'double blink',
-        'wink',
-      ])
+    it('reverse all possible actions', () => {
+      expect(commands(31)).toEqual(['jump', 'close your eyes', 'double blink', 'wink'])
     })
 
-    xit('do nothing for zero', () => {
+    it('do nothing for zero', () => {
       expect(commands(0)).toEqual([])
     })
   })
--- secret-handshake/secret-handshake.ts
+++ secret-handshake/secret-handshake.ts
@@ -1,3 +1,44 @@
-export function commands() {
-  throw new Error('Remove this statement and implement this function')
+const Event: { [key: string]: string } = {
+  1: 'wink',
+  2: 'double blink',
+  4: 'close your eyes',
+  8: 'jump'
+} as const
+
+const REVERSE_NUMBER = 16
+
+type Value<T> = T[keyof T]
+
+export function commands(decimal: number): Value<Event>[] {
+  let currNumber: number = decimal
+  let event: Value<Event>[] = []
+  let eventKeys: number[] = Object.keys(Event).map((key) => Number(key))
+
+  // Reverse eventKeys order if currNumber is greater than or equal to REVERSE_NUMBER
+  if (currNumber >= REVERSE_NUMBER) {
+    eventKeys.reverse()
+    currNumber -= REVERSE_NUMBER
+  }
+
+  // Push the corresponding event immediately if the currNumber is in the eventKeys
+  if (eventKeys.includes(currNumber)) {
+    event.push(Event[currNumber])
+    currNumber = 0
+  }
+
+  if (currNumber > 0) {
+    // Logic for determining all possible events with the given number
+    for (let i = 0; i < eventKeys.length; i++) {
+      const eventKey = eventKeys[i]
+
+      if (currNumber >= eventKey) {
+        event.push(Event[eventKey])
+        currNumber -= eventKey
+      }
+
+      if (currNumber <= 0) break
+    }
+  }
+
+  return event
 }
