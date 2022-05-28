c00a5fd75eba134bdc184743205a72a25be43d11
thewanionly <wani.pitogo@gmail.com>
2022-05-28 19:16:53 +0800

    Solved `clock` exercise, Iteration 1, Part 1 (handling conversion of raw time to hours and minutes)

--- clock/clock.ts
+++ clock/clock.ts
@@ -1,10 +1,20 @@
+const HOURS_IN_A_DAY = 24
+const MINUTES_IN_AN_HOUR = 60
+
 export class Clock {
-  constructor(hour: unknown, minute?: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  private hour: string
+  private minute: string
+
+  constructor(hour: number, minute: number = 0) {
+    // Get the total hours, including the exceeding minutes
+    const totalHours = hour + Math.floor(minute / MINUTES_IN_AN_HOUR)
+
+    this.hour = this.calculateTime(totalHours, HOURS_IN_A_DAY)
+    this.minute = this.calculateTime(minute, MINUTES_IN_AN_HOUR)
   }
 
-  public toString(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public toString(): string {
+    return `${this.hour}:${this.minute}`
   }
 
   public plus(minutes: unknown): Clock {
@@ -17,5 +27,16 @@ export class Clock {
 
   public equals(other: unknown): unknown {
     throw new Error('Remove this statement and implement this function')
+  }
+
+  private calculateTime(time: number, max: number): string {
+    // Get the absolute time. This is to handle case where time is negative
+    let absoluteTime = time + max * Math.abs(Math.floor(time / max))
+
+    // If time exceeds the max limit, roll over
+    absoluteTime = absoluteTime % max
+
+    // Convert to two digit string
+    return String(absoluteTime).padStart(2, '0')
   }
 }
