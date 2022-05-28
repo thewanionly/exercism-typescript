be251c101244ec7aac72277eda499620532d7711
thewanionly <wani.pitogo@gmail.com>
2022-05-28 19:40:51 +0800

    Solved `clock` exercise, Iteration 1, Part 2 (addition and subtraction of time)

--- clock/clock.ts
+++ clock/clock.ts
@@ -1,42 +1,59 @@
 const HOURS_IN_A_DAY = 24
 const MINUTES_IN_AN_HOUR = 60
 
+enum TimeUnit {
+  hour = 'hour',
+  minute = 'minute'
+}
+
+enum TimeUnitLimit {
+  hour = HOURS_IN_A_DAY,
+  minute = MINUTES_IN_AN_HOUR
+}
+
 export class Clock {
-  private hour: string
-  private minute: string
+  private hour: number
+  private minute: number
 
   constructor(hour: number, minute: number = 0) {
-    // Get the total hours, including the exceeding minutes
-    const totalHours = hour + Math.floor(minute / MINUTES_IN_AN_HOUR)
-
-    this.hour = this.calculateTime(totalHours, HOURS_IN_A_DAY)
-    this.minute = this.calculateTime(minute, MINUTES_IN_AN_HOUR)
+    this.hour = this.calculateTime(TimeUnit.hour, this.calculateTotalHours(hour, minute))
+    this.minute = this.calculateTime(TimeUnit.minute, minute)
   }
 
   public toString(): string {
-    return `${this.hour}:${this.minute}`
+    return `${this.toTwoDigitsString(this.hour)}:${this.toTwoDigitsString(this.minute)}`
   }
 
-  public plus(minutes: unknown): Clock {
-    throw new Error('Remove this statement and implement this function')
+  public plus(minutes: number): Clock {
+    return new Clock(this.hour, this.minute + minutes)
   }
 
-  public minus(minutes: unknown): Clock {
-    throw new Error('Remove this statement and implement this function')
+  public minus(minutes: number): Clock {
+    return new Clock(this.hour, this.minute - minutes)
   }
 
   public equals(other: unknown): unknown {
     throw new Error('Remove this statement and implement this function')
   }
 
-  private calculateTime(time: number, max: number): string {
+  private calculateTotalHours(hours: number, minutes: number): number {
+    // Get the total hours, including the exceeding minutes
+    return hours + Math.floor(minutes / MINUTES_IN_AN_HOUR)
+  }
+
+  private calculateTime(unit: TimeUnit, value: number): number {
     // Get the absolute time. This is to handle case where time is negative
-    let absoluteTime = time + max * Math.abs(Math.floor(time / max))
+    let absoluteTime =
+      value + TimeUnitLimit[unit] * Math.abs(Math.floor(value / TimeUnitLimit[unit]))
 
     // If time exceeds the max limit, roll over
-    absoluteTime = absoluteTime % max
+    absoluteTime = absoluteTime % TimeUnitLimit[unit]
 
+    return absoluteTime
+  }
+
+  private toTwoDigitsString(value: number): string {
     // Convert to two digit string
-    return String(absoluteTime).padStart(2, '0')
+    return String(value).padStart(2, '0')
   }
 }
