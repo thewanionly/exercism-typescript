1c976c4658baa4eac49807ba3b123d200c43ec26
thewanionly <wani.pitogo@gmail.com>
2022-06-01 05:55:14 +0800

    Solved `word-count` exercise Iteration 1

--- word-count/word-count.ts
+++ word-count/word-count.ts
@@ -1,3 +1,21 @@
-export function count() {
-  throw new Error('Remove this statement and implement this function')
+export function count(phrase: string): Map<string, number> {
+  const wordsArray = phrase
+    .replace(/  +/g, ' ') //collapse multiple spaces into one
+    .trim() //remove leading and trailing whitepsaces
+    .split(/\s/g) // split by whitespace (i.e. spaces, tabs and newlines)
+
+  // Store all words as keys of an object with the word count as value
+  const wordObj = wordsArray.reduce(
+    (acc: { [key: string]: number }, curr: string): { [key: string]: number } => {
+      const currWord = curr.toLowerCase()
+
+      return {
+        ...acc,
+        [currWord]: Object.prototype.hasOwnProperty.call(acc, currWord) ? acc[currWord] + 1 : 1
+      }
+    },
+    {}
+  )
+
+  return new Map(Object.entries(wordObj))
 }
