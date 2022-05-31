ae1face4b8825fcab5d50880d969476f3c4f9f9a
thewanionly <wani.pitogo@gmail.com>
2022-06-01 04:59:01 +0800

    Solved `wordy` exercise

--- wordy/wordy.test.ts
+++ wordy/wordy.test.ts
@@ -5,99 +5,93 @@ describe('Wordy', () => {
     expect(answer('What is 5?')).toEqual(5)
   })
 
-  xit('addition', () => {
+  it('addition', () => {
     expect(answer('What is 1 plus 1?')).toEqual(2)
   })
 
-  xit('more addition', () => {
+  it('more addition', () => {
     expect(answer('What is 53 plus 2?')).toEqual(55)
   })
 
-  xit('addition with negative numbers', () => {
+  it('addition with negative numbers', () => {
     expect(answer('What is -1 plus -10?')).toEqual(-11)
   })
 
-  xit('large addition', () => {
+  it('large addition', () => {
     expect(answer('What is 123 plus 45678?')).toEqual(45801)
   })
 
-  xit('subtraction', () => {
+  it('subtraction', () => {
     expect(answer('What is 4 minus -12?')).toEqual(16)
   })
 
-  xit('multiplication', () => {
+  it('multiplication', () => {
     expect(answer('What is -3 multiplied by 25?')).toEqual(-75)
   })
 
-  xit('division', () => {
+  it('division', () => {
     expect(answer('What is 33 divided by -3?')).toEqual(-11)
   })
 
-  xit('multiple additions', () => {
+  it('multiple additions', () => {
     expect(answer('What is 1 plus 1 plus 1?')).toEqual(3)
   })
 
-  xit('addition and subtraction', () => {
+  it('addition and subtraction', () => {
     expect(answer('What is 1 plus 5 minus -2?')).toEqual(8)
   })
 
-  xit('multiple subtraction', () => {
+  it('multiple subtraction', () => {
     expect(answer('What is 20 minus 4 minus 13?')).toEqual(3)
   })
 
-  xit('subtraction then addition', () => {
+  it('subtraction then addition', () => {
     expect(answer('What is 17 minus 6 plus 3?')).toEqual(14)
   })
 
-  xit('multiple multiplication', () => {
+  it('multiple multiplication', () => {
     expect(answer('What is 2 multiplied by -2 multiplied by 3?')).toEqual(-12)
   })
 
-  xit('addition and multiplication', () => {
+  it('addition and multiplication', () => {
     expect(answer('What is -3 plus 7 multiplied by -2?')).toEqual(-8)
   })
 
-  xit('multiple division', () => {
+  it('multiple division', () => {
     expect(answer('What is -12 divided by 2 divided by -3?')).toEqual(2)
   })
 
-  xit('unknown operation', () => {
-    expect(() => answer('What is 52 cubed?')).toThrow(
-      new Error('Unknown operation')
-    )
+  it('unknown operation', () => {
+    expect(() => answer('What is 52 cubed?')).toThrow(new Error('Unknown operation'))
   })
 
-  xit('Non math question', () => {
+  it('Non math question', () => {
     expect(() => answer('Who is the President of the United States?')).toThrow(
       new Error('Unknown operation')
     )
   })
 
-  xit('reject problem missing an operand', () => {
+  it('reject problem missing an operand', () => {
     expect(() => answer('What is 1 plus?')).toThrow(new Error('Syntax error'))
   })
 
-  xit('reject problem with no operands or operators', () => {
+  it('reject problem with no operands or operators', () => {
     expect(() => answer('What is?')).toThrow(new Error('Syntax error'))
   })
 
-  xit('reject two operations in a row', () => {
-    expect(() => answer('What is 1 plus plus 2?')).toThrow(
-      new Error('Syntax error')
-    )
+  it('reject two operations in a row', () => {
+    expect(() => answer('What is 1 plus plus 2?')).toThrow(new Error('Syntax error'))
   })
 
-  xit('reject two numbers in a row', () => {
-    expect(() => answer('What is 1 plus 2 1?')).toThrow(
-      new Error('Syntax error')
-    )
+  it('reject two numbers in a row', () => {
+    expect(() => answer('What is 1 plus 2 1?')).toThrow(new Error('Syntax error'))
   })
 
-  xit('reject postfix notation', () => {
+  it('reject postfix notation', () => {
     expect(() => answer('What is 1 2 plus?')).toThrow(new Error('Syntax error'))
   })
 
-  xit('reject prefix notation', () => {
+  it('reject prefix notation', () => {
     expect(() => answer('What is plus 1 2?')).toThrow(new Error('Syntax error'))
   })
 })
--- wordy/wordy.ts
+++ wordy/wordy.ts
@@ -1,3 +1,75 @@
-export const answer = () => {
-  throw new Error('Remove this statement and implement this function')
+const OPERATIONS = {
+  plus: 'plus',
+  minus: 'minus',
+  multiplied: 'multiplied',
+  divided: 'divided'
+} as const
+
+type Operation = keyof typeof OPERATIONS
+
+const MATH_QUESTION_INDICATOR = 'What'
+
+const isCharANumber = (char: string): boolean => /^-?\d+$/.test(char)
+
+const operate = (x: number, y: number, operation: Operation): number | never => {
+  switch (operation) {
+    case OPERATIONS.plus:
+      return x + y
+    case OPERATIONS.minus:
+      return x - y
+    case OPERATIONS.multiplied:
+      return x * y
+    case OPERATIONS.divided:
+      return x / y
+    default:
+      throw new Error('Unknown operation')
+  }
+}
+
+export const answer = (word: string): number | never => {
+  const wordArray = word.slice(0, -1).split(' ')
+  const numbers: number[] = []
+  const operations: Operation[] = []
+  let result = 0
+
+  // Parse numbers and operations
+  for (let i = 0; i < wordArray.length; i++) {
+    const currWord: string | Operation = wordArray[i]
+
+    // Check for numbers
+    if (isCharANumber(currWord)) {
+      numbers.push(Number(currWord))
+      continue
+    }
+
+    // Check for operations
+    if (Object.keys(OPERATIONS).includes(currWord)) {
+      if (numbers.length - operations.length === 1) {
+        operations.push(currWord as Operation)
+        continue
+      } else {
+        throw new Error('Syntax error')
+      }
+    }
+
+    // Check for unknown operation
+    if (
+      (i === 0 && currWord !== MATH_QUESTION_INDICATOR) || //check first word to determine if it is non-math question
+      isCharANumber(wordArray[i - 1]) //if operations checking above fails and previous word is a number, it means the next word after a number is not an operation
+    ) {
+      throw new Error('Unknown operation')
+    }
+  }
+
+  // Calculate result
+  if (numbers.length - operations.length === 1) {
+    result = numbers.shift() || 0
+    for (let i = 0; i < operations.length; i++) {
+      result = operate(result, numbers[i], operations[i])
+    }
+  } else {
+    throw new Error('Syntax error')
+  }
+
+  return result
 }
