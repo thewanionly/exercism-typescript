1068295a9ffbb85e2e58753d9dbc13c89c630143
thewanionly <wani.pitogo@gmail.com>
2022-06-01 06:01:18 +0800

    Solved `word-count` exercise Iteration 2 (utilize Map's property)

--- word-count/word-count.ts
+++ word-count/word-count.ts
@@ -2,20 +2,13 @@ export function count(phrase: string): Map<string, number> {
   const wordsArray = phrase
     .replace(/  +/g, ' ') //collapse multiple spaces into one
     .trim() //remove leading and trailing whitepsaces
+    .toLowerCase()
     .split(/\s/g) // split by whitespace (i.e. spaces, tabs and newlines)
 
-  // Store all words as keys of an object with the word count as value
-  const wordObj = wordsArray.reduce(
-    (acc: { [key: string]: number }, curr: string): { [key: string]: number } => {
-      const currWord = curr.toLowerCase()
-
-      return {
-        ...acc,
-        [currWord]: Object.prototype.hasOwnProperty.call(acc, currWord) ? acc[currWord] + 1 : 1
-      }
-    },
-    {}
+  // Store all words as keys of a map with the word count as value
+  return wordsArray.reduce(
+    (acc: Map<string, number>, currWord: string): Map<string, number> =>
+      acc.set(currWord, (acc.get(currWord) ?? 0) + 1),
+    new Map<string, number>()
   )
-
-  return new Map(Object.entries(wordObj))
 }
