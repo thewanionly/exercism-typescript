56e17d45685b482bd7f19205bcb5185d70e6e4f8
thewanionly <wani.pitogo@gmail.com>
2022-06-02 04:25:01 +0800

    Solved `difference-of-squares` exercise

--- difference-of-squares/difference-of-squares.ts
+++ difference-of-squares/difference-of-squares.ts
@@ -1,17 +1,19 @@
 export class Squares {
-  constructor(count: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  count: number
+
+  constructor(count: number) {
+    this.count = count
   }
 
-  get sumOfSquares(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get sumOfSquares(): number {
+    return (this.count * (this.count + 1) * (2 * this.count + 1)) / 6
   }
 
-  get squareOfSum(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get squareOfSum(): number {
+    return Math.pow((this.count * (this.count + 1)) / 2, 2)
   }
 
-  get difference(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get difference(): number {
+    return this.squareOfSum - this.sumOfSquares
   }
 }
