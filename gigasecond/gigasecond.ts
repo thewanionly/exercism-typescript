41f4fa92676a924e55b96ff2ef5f963c9fb25798
thewanionly <wani.pitogo@gmail.com>
2022-06-03 02:17:00 +0800

    Solved `gigasecond` exercise

--- gigasecond/gigasecond.ts
+++ gigasecond/gigasecond.ts
@@ -1,5 +1,14 @@
+const GIGASECOND = 10 ** 9
+const MILLISECOND = 10 ** -3
+
 export class Gigasecond {
-  public date(/* Parameters go here */) {
-    throw new Error('Remove this statement and implement this function')
+  milliseconds: number
+
+  constructor(moment: Date) {
+    this.milliseconds = moment.getTime()
+  }
+
+  public date(): Date {
+    return new Date(this.milliseconds + GIGASECOND / MILLISECOND)
   }
 }
