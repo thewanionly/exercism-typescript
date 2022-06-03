9cb343344406fa2accfdc39899c0a55c9aa270e5
thewanionly <wani.pitogo@gmail.com>
2022-06-03 19:10:18 +0800

    Solved `raindrops` exercise

--- raindrops/raindrops.ts
+++ raindrops/raindrops.ts
@@ -1,3 +1,21 @@
-export function convert() {
-  throw new Error('Remove this statement and implement this function')
+export function convert(num: number): string {
+  let res = ''
+
+  if (num % 3 === 0) {
+    res = 'Pling'
+  }
+
+  if (num % 5 === 0) {
+    res += 'Plang'
+  }
+
+  if (num % 7 === 0) {
+    res += 'Plong'
+  }
+
+  if (!res.length) {
+    res = String(num)
+  }
+
+  return res
 }
