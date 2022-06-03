902015c57d5c48b789247e9336ae66c1dca3d314
thewanionly <wani.pitogo@gmail.com>
2022-06-03 16:31:11 +0800

    Solved `triangle` exercise Iteration 2 (using sorted arrays and sets)

--- triangle/triangle.ts
+++ triangle/triangle.ts
@@ -1,42 +1,25 @@
 export class Triangle {
-  side1: number
-  side2: number
-  side3: number
+  sides: number[]
+  perimeter: number
 
   constructor(...sides: number[]) {
-    const [side1, side2, side3] = sides
-
-    this.side1 = side1
-    this.side2 = side2
-    this.side3 = side3
+    this.sides = sides.sort((a, b) => a - b)
+    this.perimeter = sides.reduce((acc, curr) => acc + curr, 0)
   }
 
-  private get isValid(): boolean {
-    return (
-      this.side1 + this.side2 + this.side3 > 0 &&
-      this.side1 + this.side2 > this.side3 &&
-      this.side2 + this.side3 > this.side1 &&
-      this.side1 + this.side3 > this.side2
-    )
+  private isValid(): boolean {
+    return this.perimeter > 0 && this.sides[0] + this.sides[1] > this.sides[2]
   }
 
   get isEquilateral(): boolean {
-    return this.isValid && this.side1 === this.side2 && this.side1 === this.side3
+    return this.isValid() && new Set(this.sides).size === 1
   }
 
   get isIsosceles(): boolean {
-    return (
-      this.isValid &&
-      (this.side1 === this.side2 || this.side1 === this.side3 || this.side2 === this.side3)
-    )
+    return this.isValid() && new Set(this.sides).size <= 2
   }
 
   get isScalene(): boolean {
-    return (
-      this.isValid &&
-      this.side1 !== this.side2 &&
-      this.side1 !== this.side3 &&
-      this.side2 !== this.side3
-    )
+    return this.isValid() && new Set(this.sides).size === 3
   }
 }
