8e715d63b0636c5b94d5a863e7a5c18bd3b85f29
thewanionly <wani.pitogo@gmail.com>
2022-06-04 00:48:35 +0800

    Solved `collatz-conjecture` exercise

--- collatz-conjecture/collatz-conjecture.ts
+++ collatz-conjecture/collatz-conjecture.ts
@@ -1,3 +1,13 @@
-export function steps(count: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function steps(num: number): number {
+  if (num <= 0) throw new Error('Only positive numbers are allowed')
+
+  let count = 0
+  let currentNum = num
+
+  while (currentNum !== 1) {
+    count++
+    currentNum = currentNum % 2 === 0 ? currentNum / 2 : 3 * currentNum + 1
+  }
+
+  return count
 }
