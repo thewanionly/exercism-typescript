20ae7721cded11fb33ad0268c3b80e596f38347a
thewanionly <wani.pitogo@gmail.com>
2022-06-04 18:37:08 +0800

    Solved `hamming` exercise

--- hamming/hamming.test.ts
+++ hamming/hamming.test.ts
@@ -5,31 +5,31 @@ describe('Hamming', () => {
     expect(compute('A', 'A')).toEqual(0)
   })
 
-  xit('complete hamming distance for single nucleotide strand', () => {
+  it('complete hamming distance for single nucleotide strand', () => {
     expect(compute('A', 'G')).toEqual(1)
   })
 
-  xit('complete hamming distance for small strand', () => {
+  it('complete hamming distance for small strand', () => {
     expect(compute('AG', 'CT')).toEqual(2)
   })
 
-  xit('small hamming distance', () => {
+  it('small hamming distance', () => {
     expect(compute('AT', 'CT')).toEqual(1)
   })
 
-  xit('small hamming distance in longer strand', () => {
+  it('small hamming distance in longer strand', () => {
     expect(compute('GGACG', 'GGTCG')).toEqual(1)
   })
 
-  xit('large hamming distance', () => {
+  it('large hamming distance', () => {
     expect(compute('GATACA', 'GCATAA')).toEqual(4)
   })
 
-  xit('hamming distance in very long strand', () => {
+  it('hamming distance in very long strand', () => {
     expect(compute('GGACGGATTCTG', 'AGGACGGATTCT')).toEqual(9)
   })
 
-  xit('throws error when strands are not equal length', () => {
+  it('throws error when strands are not equal length', () => {
     expect(() => {
       compute('GGACGGATTCTG', 'AGGAC')
     }).toThrowError('DNA strands must be of equal length.')
--- hamming/hamming.ts
+++ hamming/hamming.ts
@@ -1,3 +1,18 @@
-export function compute(left: unknown, right: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function compute(left: string, right: string): number | never {
+  // throw an error if left and right does not have the same length
+  if (left.length !== right.length) throw new Error('DNA strands must be of equal length.')
+
+  let hammingDistance = 0
+
+  // return 0 if left and right are the same
+  if (left !== right) {
+    // compute for the hamming distance
+    for (let i = 0; i < left.length; i++) {
+      if (left[i] !== right[i]) {
+        hammingDistance++
+      }
+    }
+  }
+
+  return hammingDistance
 }
