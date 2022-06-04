4fcff834560711fe0944e4d44e537e56e688adda
thewanionly <wani.pitogo@gmail.com>
2022-06-04 18:40:24 +0800

    Solved `hamming` exercise Iteration 2

--- hamming/hamming.ts
+++ hamming/hamming.ts
@@ -2,17 +2,9 @@ export function compute(left: string, right: string): number | never {
   // throw an error if left and right does not have the same length
   if (left.length !== right.length) throw new Error('DNA strands must be of equal length.')
 
-  let hammingDistance = 0
-
   // return 0 if left and right are the same
-  if (left !== right) {
-    // compute for the hamming distance
-    for (let i = 0; i < left.length; i++) {
-      if (left[i] !== right[i]) {
-        hammingDistance++
-      }
-    }
-  }
+  if (left === right) return 0
 
-  return hammingDistance
+  // compute for the hamming distance
+  return left.split('').filter((letter, i) => letter !== right[i]).length
 }
