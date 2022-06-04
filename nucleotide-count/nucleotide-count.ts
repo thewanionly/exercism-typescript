357cf740269eec25f72220bd2006ed22981299e9
thewanionly <wani.pitogo@gmail.com>
2022-06-04 20:08:30 +0800

    Solved `nucleotide-count` exercise

--- nucleotide-count/nucleotide-count.ts
+++ nucleotide-count/nucleotide-count.ts
@@ -1,3 +1,28 @@
-export function nucleotideCounts(/* Parameters go here */) {
-  throw new Error('Remove this statement and implement this function')
+const Nucleotides = {
+  A: 'A',
+  C: 'C',
+  G: 'G',
+  T: 'T'
+}
+
+type Nucleotide = keyof typeof Nucleotides
+type NucleotideCount = {
+  [key in Nucleotide]: number
+}
+
+export function nucleotideCounts(dnaSequence: string): NucleotideCount {
+  let nucleotideCount: NucleotideCount = {
+    A: 0,
+    C: 0,
+    G: 0,
+    T: 0
+  }
+
+  for (let i = 0; i < dnaSequence.length; i++) {
+    if (!(dnaSequence[i] in Nucleotides)) throw new Error('Invalid nucleotide in strand')
+
+    nucleotideCount[dnaSequence[i] as Nucleotide] += 1
+  }
+
+  return nucleotideCount
 }
