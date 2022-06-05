3a843313897801ffc19fc31a37c3e6919a15cd1a
thewanionly <wani.pitogo@gmail.com>
2022-06-06 03:59:39 +0800

    Solved `perfect-numbers` exercise

--- perfect-numbers/perfect-numbers.test.ts
+++ perfect-numbers/perfect-numbers.test.ts
@@ -6,69 +6,69 @@ describe('Perfect numbers', () => {
     expect(classify(6)).toEqual(expected)
   })
 
-  xit('Medium perfect number is classified correctly', () => {
+  it('Medium perfect number is classified correctly', () => {
     const expected = 'perfect'
     expect(classify(28)).toEqual(expected)
   })
 
-  xit('Large perfect number is classified correctly', () => {
+  it('Large perfect number is classified correctly', () => {
     const expected = 'perfect'
     expect(classify(33550336)).toEqual(expected)
   })
 })
 
 describe('Abundant numbers', () => {
-  xit('Smallest abundant number is classified correctly', () => {
+  it('Smallest abundant number is classified correctly', () => {
     const expected = 'abundant'
     expect(classify(12)).toEqual(expected)
   })
 
-  xit('Medium abundant number is classified correctly', () => {
+  it('Medium abundant number is classified correctly', () => {
     const expected = 'abundant'
     expect(classify(30)).toEqual(expected)
   })
 
-  xit('Large abundant number is classified correctly', () => {
+  it('Large abundant number is classified correctly', () => {
     const expected = 'abundant'
     expect(classify(33550335)).toEqual(expected)
   })
 })
 
 describe('Deficient numbers', () => {
-  xit('Smallest prime deficient number is classified correctly', () => {
+  it('Smallest prime deficient number is classified correctly', () => {
     const expected = 'deficient'
     expect(classify(2)).toEqual(expected)
   })
 
-  xit('Smallest non-prime deficient number is classified correctly', () => {
+  it('Smallest non-prime deficient number is classified correctly', () => {
     const expected = 'deficient'
     expect(classify(4)).toEqual(expected)
   })
 
-  xit('Medium deficient number is classified correctly', () => {
+  it('Medium deficient number is classified correctly', () => {
     const expected = 'deficient'
     expect(classify(32)).toEqual(expected)
   })
 
-  xit('Large deficient number is classified correctly', () => {
+  it('Large deficient number is classified correctly', () => {
     const expected = 'deficient'
     expect(classify(33550337)).toEqual(expected)
   })
 
-  xit('Edge case (no factors other than itself) is classified correctly', () => {
+  it('Edge case (no factors other than itself) is classified correctly', () => {
     const expected = 'deficient'
     expect(classify(1)).toEqual(expected)
   })
 })
 
 describe('Invalid inputs', () => {
-  xit('Zero is rejected (not a natural number)', () => {
+  it('Zero is rejected (not a natural number)', () => {
     expect(() => {
       classify(0)
     }).toThrowError('Classification is only possible for natural numbers.')
   })
 
-  xit('Negative integer is rejected (not a natural number)', () => {
+  it('Negative integer is rejected (not a natural number)', () => {
     expect(() => {
       classify(-1)
     }).toThrowError('Classification is only possible for natural numbers.')
--- perfect-numbers/perfect-numbers.ts
+++ perfect-numbers/perfect-numbers.ts
@@ -1,3 +1,24 @@
-export function classify() {
-  throw new Error('Remove this statement and implement this function')
+enum Classifications {
+  perfect = 'perfect',
+  abundant = 'abundant',
+  deficient = 'deficient'
+}
+
+const alliquotSum = (num: number): number => {
+  let factors: number[] = []
+
+  for (let i = 1; i < num; i++) {
+    if (num % i === 0) factors.push(i)
+  }
+
+  return factors.reduce((acc, curr) => acc + curr, 0)
+}
+
+export function classify(num: number): Classifications | never {
+  if (num <= 0) throw new Error('Classification is only possible for natural numbers.')
+
+  if (alliquotSum(num) === num) return Classifications.perfect
+  if (alliquotSum(num) > num) return Classifications.abundant
+
+  return Classifications.deficient
 }
