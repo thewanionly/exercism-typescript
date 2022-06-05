d729af7e186dac01e5d37e781a064684a8943c07
thewanionly <wani.pitogo@gmail.com>
2022-06-06 03:29:35 +0800

    Solved `allergies` exercise

--- allergies/allergies.test.ts
+++ allergies/allergies.test.ts
@@ -9,13 +9,13 @@ describe('allergicTo', () => {
     expect(allergies.allergicTo('strawberries')).toBeFalsy()
   })
 
-  xit('is allergic to eggs', () => {
+  it('is allergic to eggs', () => {
     const allergies = new Allergies(1)
 
     expect(allergies.allergicTo('eggs')).toBeTruthy()
   })
 
-  xit('allergic to eggs in addition to other stuff', () => {
+  it('allergic to eggs in addition to other stuff', () => {
     const allergies = new Allergies(5)
 
     expect(allergies.allergicTo('eggs')).toBeTruthy()
@@ -25,56 +25,56 @@ describe('allergicTo', () => {
 })
 
 describe('list', () => {
-  xit('no allergies at all', () => {
+  it('no allergies at all', () => {
     const allergies = new Allergies(0)
     const expected: string[] = []
 
     expect(allergies.list()).toEqual(expected)
   })
 
-  xit('allergic to just eggs', () => {
+  it('allergic to just eggs', () => {
     const allergies = new Allergies(1)
     const expected = ['eggs']
 
     expect(allergies.list()).toEqual(expected)
   })
 
-  xit('allergic to just peanuts', () => {
+  it('allergic to just peanuts', () => {
     const allergies = new Allergies(2)
     const expected = ['peanuts']
 
     expect(allergies.list()).toEqual(expected)
   })
 
-  xit('allergic to just strawberries', () => {
+  it('allergic to just strawberries', () => {
     const allergies = new Allergies(8)
     const expected = ['strawberries']
 
     expect(allergies.list()).toEqual(expected)
   })
 
-  xit('allergic to eggs and peanuts', () => {
+  it('allergic to eggs and peanuts', () => {
     const allergies = new Allergies(3)
     const expected = ['eggs', 'peanuts']
 
     expect(allergies.list()).toEqual(expected)
   })
 
-  xit('allergic to more than eggs but not peanuts', () => {
+  it('allergic to more than eggs but not peanuts', () => {
     const allergies = new Allergies(5)
     const expected = ['eggs', 'shellfish']
 
     expect(allergies.list()).toEqual(expected)
   })
 
-  xit('allergic to lots of stuff', () => {
+  it('allergic to lots of stuff', () => {
     const allergies = new Allergies(248)
     const expected = ['strawberries', 'tomatoes', 'chocolate', 'pollen', 'cats']
 
     expect(allergies.list()).toEqual(expected)
   })
 
-  xit('allergic to everything', () => {
+  it('allergic to everything', () => {
     const allergies = new Allergies(255)
     const expected = [
       'eggs',
@@ -84,13 +84,13 @@ describe('list', () => {
       'tomatoes',
       'chocolate',
       'pollen',
-      'cats',
+      'cats'
     ]
 
     expect(allergies.list()).toEqual(expected)
   })
 
-  xit('ignore non allergen score parts', () => {
+  it('ignore non allergen score parts', () => {
     const allergies = new Allergies(509)
     const expected = [
       'eggs',
@@ -99,7 +99,7 @@ describe('list', () => {
       'tomatoes',
       'chocolate',
       'pollen',
-      'cats',
+      'cats'
     ]
 
     expect(allergies.list()).toEqual(expected)
--- allergies/allergies.ts
+++ allergies/allergies.ts
@@ -1,13 +1,50 @@
+const Allergens: { [key: string]: number } = {
+  cats: 128,
+  pollen: 64,
+  chocolate: 32,
+  tomatoes: 16,
+  strawberries: 8,
+  shellfish: 4,
+  peanuts: 2,
+  eggs: 1
+}
+
+const ExcludedAllergenScores = [1024, 512, 256]
+
+type Allergen = keyof typeof Allergens
+
 export class Allergies {
-  constructor(allergenIndex: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  allergens: Allergen[] = []
+
+  constructor(allergenIndex: number) {
+    let currAllergenScore = allergenIndex
+    let excludedAllergenScore =
+      ExcludedAllergenScores.find((score) => currAllergenScore > score) || 0
+
+    // deduct the max excluded allergen score from currAllergenScore
+    while (excludedAllergenScore) {
+      currAllergenScore -= excludedAllergenScore
+      excludedAllergenScore = ExcludedAllergenScores.find((score) => currAllergenScore > score) || 0
+    }
+
+    // loop over the allergens and based on the currAllergenScore, create the allergens list
+    if (allergenIndex > 0) {
+      for (let allergen in Allergens) {
+        const allergenScore = Allergens[allergen]
+
+        if (allergenScore <= currAllergenScore) {
+          this.allergens.unshift(allergen)
+          currAllergenScore -= allergenScore
+        }
+      }
+    }
   }
 
-  public list(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public list(): Allergen[] {
+    return this.allergens
   }
 
-  public allergicTo(allergen: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public allergicTo(allergen: Allergen): boolean {
+    return this.allergens.includes(allergen)
   }
 }
