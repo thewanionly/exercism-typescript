7e4df5efcb661f8e837ced3a40cf9381ff46e259
thewanionly <wani.pitogo@gmail.com>
2022-06-07 20:13:28 +0800

    Solved `grains` exercise Iteration 1

--- grains/grains.ts
+++ grains/grains.ts
@@ -1,7 +1,17 @@
-export const square = () => {
-  throw new Error('Remove this statement and implement this function')
+const MAX_SQUARES = 64
+
+export const square = (squareNum: number): bigint | never => {
+  if (squareNum <= 0 || squareNum > MAX_SQUARES) throw new Error()
+
+  return BigInt(2 ** (squareNum - 1))
 }
 
-export const total = () => {
-  throw new Error('Remove this statement and implement this function')
+export const total = (): bigint => {
+  let totalGrains = BigInt(0)
+
+  for (let i = 0; i < MAX_SQUARES; i++) {
+    totalGrains += BigInt(2 ** i)
+  }
+
+  return totalGrains
 }
