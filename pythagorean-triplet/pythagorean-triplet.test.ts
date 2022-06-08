501eb7248d4db39e4b80f2e19372e9c45d987b6e
thewanionly <wani.pitogo@gmail.com>
2022-06-09 01:54:08 +0800

    Solved `pythagorean-triplet` exercise Iteration 1

--- pythagorean-triplet/pythagorean-triplet.test.ts
+++ pythagorean-triplet/pythagorean-triplet.test.ts
@@ -3,9 +3,7 @@ import { triplets } from './pythagorean-triplet'
 type Triplet = [number, number, number]
 
 function tripletsWithSum(sum: number, options = {}): Triplet[] {
-  return triplets({ ...options, sum }).map((triplet) =>
-    triplet.toArray().sort((a, b) => a - b)
-  )
+  return triplets({ ...options, sum }).map((triplet) => triplet.toArray().sort((a, b) => a - b))
 }
 
 describe('Triplet', () => {
@@ -13,26 +11,26 @@ describe('Triplet', () => {
     expect(tripletsWithSum(12)).toEqual([[3, 4, 5]])
   })
 
-  xit('triplets whose sum is 108', () => {
+  it('triplets whose sum is 108', () => {
     expect(tripletsWithSum(108)).toEqual([[27, 36, 45]])
   })
 
-  xit('triplets whose sum is 1000', () => {
+  it('triplets whose sum is 1000', () => {
     expect(tripletsWithSum(1000)).toEqual([[200, 375, 425]])
   })
 
-  xit('no matching triplets for 1001', () => {
+  it('no matching triplets for 1001', () => {
     expect(tripletsWithSum(1001)).toEqual([])
   })
 
-  xit('returns all matching triplets', () => {
+  it('returns all matching triplets', () => {
     expect(tripletsWithSum(90)).toEqual([
       [9, 40, 41],
-      [15, 36, 39],
+      [15, 36, 39]
     ])
   })
 
-  xit('several matching triplets', () => {
+  it('several matching triplets', () => {
     expect(tripletsWithSum(840)).toEqual([
       [40, 399, 401],
       [56, 390, 394],
@@ -41,22 +39,22 @@ describe('Triplet', () => {
       [140, 336, 364],
       [168, 315, 357],
       [210, 280, 350],
-      [240, 252, 348],
+      [240, 252, 348]
     ])
   })
 
-  xit('returns triplets with no factor smaller than minimum factor', () => {
+  it('returns triplets with no factor smaller than minimum factor', () => {
     expect(tripletsWithSum(90, { minFactor: 10 })).toEqual([[15, 36, 39]])
   })
 
-  xit('returns triplets with no factor larger than maximum factor', () => {
+  it('returns triplets with no factor larger than maximum factor', () => {
     expect(tripletsWithSum(840, { maxFactor: 349 })).toEqual([[240, 252, 348]])
   })
 
-  xit('returns triplets with factors in range', () => {
+  it('returns triplets with factors in range', () => {
     expect(tripletsWithSum(840, { maxFactor: 352, minFactor: 150 })).toEqual([
       [210, 280, 350],
-      [240, 252, 348],
+      [240, 252, 348]
     ])
   })
 
@@ -66,7 +64,7 @@ describe('Triplet', () => {
       [1875, 14000, 14125],
       [5000, 12000, 13000],
       [6000, 11250, 12750],
-      [7500, 10000, 12500],
+      [7500, 10000, 12500]
     ])
   })
 })
--- pythagorean-triplet/pythagorean-triplet.ts
+++ pythagorean-triplet/pythagorean-triplet.ts
@@ -4,16 +4,32 @@ type Options = {
   sum: number
 }
 
-export function triplets({}: Options): Triplet[] {
-  throw new Error('Remove this statement and implement this function')
-}
-
 class Triplet {
-  constructor() {
-    throw new Error('Remove this statement and implement this function')
-  }
+  constructor(private a: number, private b: number, private c: number) {}
 
   toArray(): [number, number, number] {
-    throw new Error('Remove this statement and implement this function')
+    return [this.a, this.b, this.c]
   }
+}
+
+export function triplets({ sum, minFactor = 1, maxFactor }: Options): Triplet[] {
+  const tripletsArr: Triplet[] = []
+
+  // Find all 3 numbers that sum up to `sum`
+  for (let a = minFactor; a <= sum - 2; a++) {
+    for (let b = a + 1; b <= sum - a; b++) {
+      const cLimit = maxFactor || sum - (a + b)
+
+      for (let c = b + 1; c <= cLimit; c++) {
+        const N = a + b + c
+
+        if (N > sum) break
+
+        if (N === sum && a ** 2 + b ** 2 === c ** 2) {
+          tripletsArr.push(new Triplet(a, b, c))
+        }
+      }
+    }
+  }
+  return tripletsArr
 }
