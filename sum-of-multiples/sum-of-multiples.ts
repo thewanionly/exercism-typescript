2a6dbd17053e92f5b759cb6f8b82b0d857a3de33
thewanionly <wani.pitogo@gmail.com>
2022-06-09 23:56:56 +0800

    Solved `sum-of-multiples` exercise Iteration 1

--- sum-of-multiples/sum-of-multiples.ts
+++ sum-of-multiples/sum-of-multiples.ts
@@ -1,3 +1,13 @@
-export function sum() {
-  throw new Error('Remove this statement and implement this function')
+export function sum(multiples: number[], limit: number): number {
+  const numbers = new Set<number>()
+
+  for (let i = 1; i < limit; i++) {
+    for (let multiple of multiples) {
+      if (i % multiple === 0) {
+        numbers.add(i)
+      }
+    }
+  }
+
+  return [...numbers].reduce((acc, curr) => acc + curr, 0)
 }
