d7a1cc592d25afabffcb3974297540762f91da29
thewanionly <wani.pitogo@gmail.com>
2022-06-12 17:47:28 +0800

    Solved `series` exercise

--- series/series.ts
+++ series/series.ts
@@ -1,9 +1,20 @@
 export class Series {
-  constructor(series: unknown) {
-    throw new Error('Remove this statement and implement this function')
-  }
+  constructor(private series: string) {}
 
-  slices(sliceLength: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+  slices(sliceLength: number): number[][] {
+    //Error checking
+    if (!this.series) throw new Error('series cannot be empty')
+    if (sliceLength > this.series.length)
+      throw new Error('slice length cannot be greater than series length')
+    if (sliceLength === 0) throw new Error('slice length cannot be zero')
+    if (sliceLength < 0) throw new Error('slice length cannot be negative')
+
+    const results: string[] = []
+
+    for (let i = 0; i <= this.series.length - sliceLength; i++) {
+      results.push(this.series.substring(i, i + sliceLength))
+    }
+
+    return results.map((res) => res.split('').map((numStr) => Number(numStr)))
   }
 }
