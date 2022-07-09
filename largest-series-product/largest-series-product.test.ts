1f28611c2b5b768f3d2a206192d6c01edfac3b70
thewanionly <wani.pitogo@gmail.com>
2022-07-09 23:34:39 +0800

    Solved `largest-series-product` exercise

--- largest-series-product/largest-series-product.test.ts
+++ largest-series-product/largest-series-product.test.ts
@@ -5,69 +5,65 @@ describe('Largest Series Product', () => {
     expect(largestProduct('29', 2)).toEqual(18)
   })
 
-  xit('can find the largest product of 2 with numbers in order', () => {
+  it('can find the largest product of 2 with numbers in order', () => {
     expect(largestProduct('0123456789', 2)).toEqual(72)
   })
 
-  xit('can find the largest product of 2', () => {
+  it('can find the largest product of 2', () => {
     expect(largestProduct('576802143', 2)).toEqual(48)
   })
 
-  xit('can find the largest product of 3 with numbers in order', () => {
+  it('can find the largest product of 3 with numbers in order', () => {
     expect(largestProduct('0123456789', 3)).toEqual(504)
   })
 
-  xit('can find the largest product of 3', () => {
+  it('can find the largest product of 3', () => {
     expect(largestProduct('1027839564', 3)).toEqual(270)
   })
 
-  xit('can find the largest product of 5 with numbers in order', () => {
+  it('can find the largest product of 5 with numbers in order', () => {
     expect(largestProduct('0123456789', 5)).toEqual(15120)
   })
 
-  xit('can get the largest product of a big number', () => {
-    expect(
-      largestProduct('73167176531330624919225119674426574742355349194934', 6)
-    ).toEqual(23520)
+  it('can get the largest product of a big number', () => {
+    expect(largestProduct('73167176531330624919225119674426574742355349194934', 6)).toEqual(23520)
   })
 
-  xit('reports zero if the only digits are zero', () => {
+  it('reports zero if the only digits are zero', () => {
     expect(largestProduct('0000', 2)).toEqual(0)
   })
 
-  xit('reports zero if all spans include zero', () => {
+  it('reports zero if all spans include zero', () => {
     expect(largestProduct('99099', 3)).toEqual(0)
   })
 
-  xit('rejects span longer than string length', () => {
+  it('rejects span longer than string length', () => {
     expect(() => largestProduct('123', 4)).toThrow(
       new Error('Span must be smaller than string length')
     )
   })
 
-  xit('reports 1 for empty string and empty product (0 span)', () => {
+  it('reports 1 for empty string and empty product (0 span)', () => {
     expect(largestProduct('', 0)).toEqual(1)
   })
 
-  xit('reports 1 for nonempty string and empty product (0 span)', () => {
+  it('reports 1 for nonempty string and empty product (0 span)', () => {
     expect(largestProduct('123', 0)).toEqual(1)
   })
 
-  xit('rejects empty string and nonzero span', () => {
+  it('rejects empty string and nonzero span', () => {
     expect(() => largestProduct('', 1)).toThrow(
       new Error('Span must be smaller than string length')
     )
   })
 
-  xit('rejects invalid character in digits', () => {
+  it('rejects invalid character in digits', () => {
     expect(() => largestProduct('1234a5', 2)).toThrow(
       new Error('Digits input must only contain digits')
     )
   })
 
-  xit('rejects negative span', () => {
-    expect(() => largestProduct('12345', -1)).toThrow(
-      new Error('Span must be greater than zero')
-    )
+  it('rejects negative span', () => {
+    expect(() => largestProduct('12345', -1)).toThrow(new Error('Span must be greater than zero'))
   })
 })
--- largest-series-product/largest-series-product.ts
+++ largest-series-product/largest-series-product.ts
@@ -1,3 +1,23 @@
-export const largestProduct = () => {
-  throw new Error('Remove this statement and implement this function')
+export const largestProduct = (input: string, digitslength: number): number => {
+  //validity check
+  if (input.length < digitslength) throw new Error('Span must be smaller than string length')
+  if (digitslength < 0) throw new Error('Span must be greater than zero')
+  if (input.split('').some((char) => !/^-?\d+$/.test(char)))
+    throw new Error('Digits input must only contain digits')
+
+  let largestProd = 0
+
+  for (let i = 0; i <= input.length - digitslength; i++) {
+    let currProduct = 1
+
+    for (let j = i; j < i + digitslength; j++) {
+      currProduct *= Number(input.charAt(j))
+    }
+
+    if (!largestProd || currProduct > largestProd) {
+      largestProd = currProduct
+    }
+  }
+
+  return largestProd
 }
